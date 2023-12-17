d42e7aac5bd00a06ea08931067e160ad2448f646
kimkangtae <tt997768@gmail.com>
2023-12-18 03:41:31 +0900

    page 분할

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'my_home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -21,72 +23,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '홈',
-          style: TextStyle(
-            color: Colors.blue,
-            fontSize: 30,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '숫자',
-              style: TextStyle(
-                color: Colors.black12,
-                fontSize: 70,
-              ),
-            ),
-            Text(
-              '$count',
-              style: TextStyle(
-                color: Colors.redAccent,
-                fontSize: 70,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('Button Tapped');
-              },
-              child: Text('ElevatedButton'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('TextButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('TextButton'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 화면 갱신
-          setState(() {
-            count++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
---
+++ lib/my_home_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// Camel Case (낙타형)
+// snake _case (뱀형)
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '홈',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 30,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '숫자',
+              style: TextStyle(
+                color: Colors.black12,
+                fontSize: 70,
+              ),
+            ),
+            Text(
+              '$count',
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 70,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Button Tapped');
+              },
+              child: Text('ElevatedButton'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('TextButton'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('TextButton'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // 화면 갱신
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
